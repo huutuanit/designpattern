350066419e9c49d8e1f06c1a7ff2e46271b78acf
unknown <huutuanit852000@gmail.com>
2022-03-16 17:27:55 +0700

    Adapter Patterns

---
+++ src/adapter/Customer.java
@@ -0,0 +1,6 @@
+package adapter;
+
+public interface Customer {
+
+	public void sendRequest(String request);
+}
---
+++ src/adapter/Developer.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public class Developer {
+
+	public void receive(String info) {
+		System.out.println("Receiving infomation from Adapter ...");
+		System.out.println(info);
+	}
+}
